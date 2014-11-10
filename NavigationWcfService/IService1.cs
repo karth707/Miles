ed0b0a0ca0e495186d4a1c664622d4ae8e50bdec
karth707 <kartheek.nallepalli@gmail.com>
2014-11-09 20:52:52 -0700

    added navigation

---
+++ NavigationWcfService/IService1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace NavigationWcfService
+{
+    [ServiceContract]
+    public interface IService1
+    {
+
+        [OperationContract]
+        String GetDistance(String start, String end);
+
+    }
+}
---
+++ NavigationWcfService/Service1.svc.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Xml;
+
+namespace NavigationWcfService
+{   
+    public class Service1 : IService1
+    {
+        private static String API_KEY = "AIzaSyDDZjOK5uhwbe_infKfinmjE9x26hQMUIg";
+
+        public string GetDistance(String start, String end)
+        {
+            string url = @"https://maps.googleapis.com/maps/api/distancematrix/xml?origins=" +
+              start + "&destinations=" + end +
+              "&mode=driving&sensor=false&language=en-EN&units=imperial" + "&key=" + API_KEY;
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            WebResponse response = request.GetResponse();
+            Stream dataStream = response.GetResponseStream();
+            StreamReader sreader = new StreamReader(dataStream);
+            string responsereader = sreader.ReadToEnd();
+            response.Close();
+
+            XmlDocument xmldoc = new XmlDocument();
+            xmldoc.LoadXml(responsereader);
+
+
+            if (xmldoc.GetElementsByTagName("status")[0].ChildNodes[0].InnerText == "OK")
+            {
+                XmlNodeList distance = xmldoc.GetElementsByTagName("distance");
+                return distance[0].ChildNodes[1].InnerText;
+            }
+            return "0 mi";
+        }       
+    }
+}
